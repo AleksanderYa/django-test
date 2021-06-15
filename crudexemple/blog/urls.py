69397affc17b3058e5ba814fbe0380e6849662ad
AleksanderYa <250000ye@gmail.com>
2021-06-15 17:26:51 +0300

    endDay 15.06

--- crudexemple/blog/urls.py
+++ crudexemple/blog/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post-list'),
-
+    path('lol/', views.post_list_two, name='post-list-two'),
 ]
--- crudexemple/blog/views.py
+++ crudexemple/blog/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def post_list(request):
     return render(request, 'blog/post_list.html')
+
+def post_list_two(request):
+    return render(request, 'blog/post_list_two.html')
